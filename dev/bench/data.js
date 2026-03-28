0b9dea62b70d519590cff2dd38925bda0ee7a1c3
github-action-benchmark <github@users.noreply.github.com>
2026-03-28 06:03:39 +0000

    add Repomix Performance (customSmallerIsBetter) benchmark result for 8827b336703cadb127ba0fafa6f25b19abba5fe1

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774677819276,
+  "repoUrl": "https://github.com/DreDay4u/repomix",
+  "entries": {
+    "Repomix Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "koukun0120@gmail.com",
+            "name": "Kazuki Yamada",
+            "username": "yamadashy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8827b336703cadb127ba0fafa6f25b19abba5fe1",
+          "message": "Merge pull request #1326 from yamadashy/renovate/website-non-major-dependencies\n\nfix(deps): update website non-major dependencies",
+          "timestamp": "2026-03-28T14:44:29+09:00",
+          "tree_id": "f5459998984fde90dc23ad49ae6b275ae0f194bb",
+          "url": "https://github.com/DreDay4u/repomix/commit/8827b336703cadb127ba0fafa6f25b19abba5fe1"
+        },
+        "date": 1774677819011,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Repomix Pack (macOS)",
+            "value": 2125,
+            "range": "±327",
+            "unit": "ms",
+            "extra": "Median of 20 runs\nQ1: 1967ms, Q3: 2294ms\nAll times: 1729, 1897, 1909, 1957, 1960, 1967, 2016, 2039, 2066, 2117, 2125, 2167, 2216, 2253, 2266, 2294, 2300, 2417, 2607, 2653ms"
+          },
+          {
+            "name": "Repomix Pack (Linux)",
+            "value": 2963,
+            "range": "±24",
+            "unit": "ms",
+            "extra": "Median of 10 runs\nQ1: 2947ms, Q3: 2971ms\nAll times: 2934, 2940, 2947, 2953, 2960, 2963, 2967, 2971, 2976, 2990ms"
+          },
+          {
+            "name": "Repomix Pack (Windows)",
+            "value": 3548,
+            "range": "±33",
+            "unit": "ms",
+            "extra": "Median of 10 runs\nQ1: 3526ms, Q3: 3559ms\nAll times: 3511, 3517, 3526, 3531, 3534, 3548, 3556, 3559, 3655, 3704ms"
+          }
+        ]
+      }
+    ]
+  }
+}
